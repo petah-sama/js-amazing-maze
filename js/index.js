00fb82dfa37b17e68bcf4a5c773e9021c20f964f
Pedro Batista <pedrobatista.420@gmail.com>
2020-10-07 18:25:38 +0100

    refactored everything so its reponsive and spawns the entire screen, and added some basic colors

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 3;
-const width = 600;
-const height = 600;
-const unitLength = width / cells;
+const cellsHorizontal = 4;
+const cellsVertical = 3;
+const width = window.innerWidth;
+const height = window.innerHeight;
+const unitLengthX = width / cellsHorizontal;
+const unitLengthY = height / cellsVertical;
 
 const engine = Engine.create();
 engine.world.gravity.y = 0;
@@ -12,7 +14,7 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        wireframes: true,
+        wireframes: false,
         width,
         height
     }
@@ -21,7 +23,7 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-// WALLS
+// BORDER WALLS
 const walls = [
     Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
     Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
@@ -46,20 +48,20 @@ const shuffle = (arr) => {
     return arr;
 };
 
-const grid = Array(cells)
+const grid = Array(cellsVertical)
     .fill(null)
-    .map(() => Array(cells).fill(false));
+    .map(() => Array(cellsHorizontal).fill(false));
 
-const verticals = Array(cells)
+const verticals = Array(cellsVertical)
     .fill(null)
-    .map(() => Array(cells - 1).fill(false));
+    .map(() => Array(cellsHorizontal - 1).fill(false));
 
-const horizontals = Array(cells - 1)
+const horizontals = Array(cellsVertical - 1)
     .fill(null)
-    .map(() => Array(cells).fill(false));
+    .map(() => Array(cellsHorizontal).fill(false));
 
-const startRow = Math.floor(Math.random() * cells);
-const startColumn = Math.floor(Math.random() * cells);
+const startRow = Math.floor(Math.random() * cellsVertical);
+const startColumn = Math.floor(Math.random() * cellsHorizontal);
 
 const visitCell = (row, column) => {
     // If I have visited the cell at [row, column], then return
@@ -82,7 +84,12 @@ const visitCell = (row, column) => {
         const [nextRow, nextColumn, direction] = neighbour;
         
         // See if that neighbour is out of bounds
-        if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+        if (
+            nextRow < 0 || 
+            nextRow >= cellsVertical || 
+            nextColumn < 0 || 
+            nextColumn >= cellsHorizontal
+        ) {
             continue;
         }
         
@@ -121,13 +128,16 @@ horizontals.forEach((row, rowIndex) => {
         }
 
         const wall = Bodies.rectangle(
-            columnIndex * unitLength + unitLength / 2,
-            rowIndex * unitLength + unitLength,
-            unitLength,
+            columnIndex * unitLengthX + unitLengthX / 2,
+            rowIndex * unitLengthY + unitLengthY,
+            unitLengthX,
             5,
             {
                 isStatic: true,
-                label: 'wall'
+                label: 'wall',
+                render: {
+                    fillStyle: 'red'
+                }
             }
         );
         World.add(world, wall);
@@ -141,13 +151,16 @@ verticals.forEach((row, rowIndex) => {
         }
 
         const wall = Bodies.rectangle(
-            columnIndex * unitLength + unitLength,
-            rowIndex * unitLength + unitLength / 2,
+            columnIndex * unitLengthX + unitLengthX,
+            rowIndex * unitLengthY + unitLengthY / 2,
             5,
-            unitLength,
+            unitLengthY,
             {
                 isStatic: true,
-                label: 'wall'
+                label: 'wall',
+                render: {
+                    fillStyle: 'red'
+                }
             }
         );
         World.add(world, wall);
@@ -156,25 +169,32 @@ verticals.forEach((row, rowIndex) => {
 
 // GOAL
 const goal = Bodies.rectangle(
-    width - unitLength / 2,
-    height - unitLength / 2,
-    unitLength * .5,
-    unitLength * .5,
+    width - unitLengthX / 2,
+    height - unitLengthY / 2,
+    unitLengthX * .5,
+    unitLengthY * .5,
     {
         isStatic: true,
-        label: 'goal'
+        label: 'goal',
+        render: {
+            fillStyle: 'green'
+        }
     }
 );
 
 World.add(world, goal);
 
 // BALL
+const ballRadius = Math.min(unitLengthX, unitLengthY) / 4;
 const ball = Bodies.circle(
-    unitLength / 2,
-    unitLength / 2,
-    unitLength / 4,
+    unitLengthX / 2,
+    unitLengthY / 2,
+    ballRadius,
     {
-        label: 'ball'
+        label: 'ball',
+        render: {
+            fillStyle: 'blue'
+        }
     }
 );
 
