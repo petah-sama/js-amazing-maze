1e1f17139f840510bf05759a42fd7c04811b5766
Pedro Batista <pedrobatista.420@gmail.com>
2020-10-07 18:01:58 +0100

    printed walls on the maze, the ball and the goal. added ball keys movement and samll animation on win

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-const { Engine, Render, Runner, World, Bodies, Mouse, MouseConstraint } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 const cells = 3;
 const width = 600;
 const height = 600;
+const unitLength = width / cells;
 
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
     element: document.body,
@@ -19,21 +21,16 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}));
-
-// Walls
+// WALLS
 const walls = [
-    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 2, height, { isStatic: true })
 ];
 World.add(world, walls);
 
-// Maze Generation
-
+// MAZE GENERATION
 const shuffle = (arr) => {
     let counter = arr.length;
 
@@ -117,3 +114,105 @@ const visitCell = (row, column) => {
 
 visitCell(startRow, startColumn);
 
+horizontals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength / 2,
+            rowIndex * unitLength + unitLength,
+            unitLength,
+            5,
+            {
+                isStatic: true,
+                label: 'wall'
+            }
+        );
+        World.add(world, wall);
+    });
+});
+
+verticals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength,
+            rowIndex * unitLength + unitLength / 2,
+            5,
+            unitLength,
+            {
+                isStatic: true,
+                label: 'wall'
+            }
+        );
+        World.add(world, wall);
+    });
+});
+
+// GOAL
+const goal = Bodies.rectangle(
+    width - unitLength / 2,
+    height - unitLength / 2,
+    unitLength * .5,
+    unitLength * .5,
+    {
+        isStatic: true,
+        label: 'goal'
+    }
+);
+
+World.add(world, goal);
+
+// BALL
+const ball = Bodies.circle(
+    unitLength / 2,
+    unitLength / 2,
+    unitLength / 4,
+    {
+        label: 'ball'
+    }
+);
+
+World.add(world, ball);
+
+document.addEventListener('keydown', (event) => {
+    const { x, y } = ball.velocity;
+
+    if ( event.key === 'ArrowUp' || event.key === 'w') {
+        Body.setVelocity(ball, { x, y: y - 5 });
+    }
+    if ( event.key === 'ArrowRight' || event.key === 'd') {
+        Body.setVelocity(ball, { x: x + 5, y });
+    }
+    if ( event.key === 'ArrowDown' || event.key === 's') {
+        Body.setVelocity(ball, { x, y: y + 5 });
+    }
+    if ( event.key === 'ArrowLeft' || event.key === 'a') {
+        Body.setVelocity(ball, { x: x - 5, y });
+    }
+});
+
+// WIN CONDITION
+
+Events.on(engine, 'collisionStart', event => {
+    event.pairs.forEach(collision => {
+        const labels = ['ball', 'goal'];
+
+        if (
+            labels.includes(collision.bodyA.label) && 
+            labels.includes(collision.bodyB.label)
+        ) {
+            world.gravity.y = 1;
+            world.bodies.forEach((body) => {
+                if (body.label === 'wall' || body.label === 'goal') {
+                    Body.setStatic(body, false)
+                }
+            });
+        }
+    });
+});
